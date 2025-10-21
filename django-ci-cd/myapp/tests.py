06d445634333df56e1edf81e59ba6dff8351c686
aaronmeastwood <227181718+aaronmeastwood-code@users.noreply.github.com>
2025-10-21 21:29:25 +1000

    fixing tests by comments

--- django-ci-cd/myapp/tests.py
+++ django-ci-cd/myapp/tests.py
@@ -2,8 +2,8 @@
 from django.test import TestCase
 
 class SimpleTest(TestCase):
-    def test_addition(self):
-        self.assertEqual(1 + 1, 2)  # Passing test
+   # def test_addition(self):
+       # self.assertEqual(1 + 1, 2)  # Passing test
 
     def test_failure_example(self):
         self.assertEqual(2 * 2, 5)  # Intentional fail
