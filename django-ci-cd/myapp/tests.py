6aebc4902caa2d1f7e575d3ed5bf586e14dabc28
aaronmeastwood-code <227181718+aaronmeastwood-code@users.noreply.github.com>
2025-10-21 21:11:30 +1000

    Update tests.py

--- django-ci-cd/myapp/tests.py
+++ django-ci-cd/myapp/tests.py
@@ -1,2 +1,9 @@
-def test_failure_example(self):
-    self.assertEqual(2 * 2, 5)  # intentional fail
+# django-ci-cd/myapp/tests.py
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)  # Passing test
+
+    def test_failure_example(self):
+        self.assertEqual(2 * 2, 5)  # Intentional fail
